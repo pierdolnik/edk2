90acd40de2d979ae2574aff306ee175c351ebb98
lix <95425619+lixkote@users.noreply.github.com>
2026-05-19 07:03:43 +0200

    Remove ugly "press ESC" text on splash

--- UefiPayloadPkg/Library/PlatformBootManagerLib/PlatformBootManager.c
+++ UefiPayloadPkg/Library/PlatformBootManagerLib/PlatformBootManager.c
@@ -506,7 +506,7 @@ PlatformBootManagerAfterConsole (
       BootLogoUpdateProgress (
         White,
         Black,
-        L"Press ESC for Boot Options/Settings",
+        L" ",
         White,
         0,
         0
